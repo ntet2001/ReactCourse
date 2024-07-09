f9ed28ea5a13a7a2bab105ffe12b59ea7c0680d5
ntet2001 <manyamaigor2001@gmail.com>
2024-07-09 03:28:29 +0100

    react course started

---
+++ react-course/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['arial', 'sans-serif'],
+    },
+    gridTemplateColumns: {
+      '70/30': '70% 28%',
+    },
+  },
+  plugins: [],
+}
+
